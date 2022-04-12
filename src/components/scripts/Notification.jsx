12520fa85c6a6b2d50d120846a78226627f3cafb
alokkumar-dev <alokshakya2601@gmail.com>
2022-04-12 22:16:56 +0530

    modify files

--- src/components/scripts/Notification.jsx
+++ src/components/scripts/Notification.jsx
@@ -2,6 +2,7 @@ export const Notification = () => {
   return (
     <>
       <h4>this is Notification page I created it leter</h4>
+      <h4>this is Notification page I created it leter</h4>
     </>
   );
 };
