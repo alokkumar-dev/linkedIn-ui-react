526ac4dcc68207f4da3a795514974f3b3a7bff8f
alokkumar-dev <alokshakya2601@gmail.com>
2022-04-15 12:45:58 +0530

    modify files

--- src/Data/PostData.js
+++ src/Data/PostData.js
@@ -4,6 +4,7 @@ import {
   faCalendar,
   faCommentDots,
   faImage,
+  faNewspaper,
   faPaperPlane,
   faThumbsUp,
   faVideo,
@@ -17,10 +18,10 @@ export const Posts_Data = [
     avatar:
       "https://media-exp1.licdn.com/dms/image/C5603AQF9Mgs6p2Mbcg/profile-displayphoto-shrink_800_800/0/1624713289242?e=1654732800&v=beta&t=yZ0SO8VCKCCd1BhMRfElFLclDFamfTg_h0SOJDo8HTM",
     about_post:
-      "#15 / 101 Power Skills -  Marathon Mindset  Power skills help individuals to # achieve their potential # achieve their aspirations Life is a marathon, not a sprint; pace yourself accordingly -Amby B A marathon mindset means that",
+      "# 18 / 101 Power Skills - Active Patience Power skills help individuals to # achieve their potential # achieve their aspirations Active patience demands actions & Good intentions while waiting for results to be achieved.",
     time: "24h",
     post_picture:
-      "https://media-exp1.licdn.com/dms/image/C5622AQGNP8ibd4ohRA/feedshare-shrink_800/0/1649242104156?e=2147483647&v=beta&t=M2BXLaf8iciWDeP1HZxB0D2qsf8EMQCyL0I6o0FTxxY",
+      "https://media-exp1.licdn.com/dms/image/C5622AQE-pWsM1rZhWg/feedshare-shrink_800/0/1649867369564?e=2147483647&v=beta&t=YzNkBS3nB_MqADk92m8iSRAW74xQsYKY4xNija-P7f8",
     likes: "Devesh Verma and 99 others",
     comments: "20 comments",
     icons: [
@@ -232,19 +233,19 @@ export const Posts_Data = [
 
 export const NAVBAR_ICONS = [
   {
-    icon: <FontAwesomeIcon className="fa-solid" icon={faImage} />,
+    icon: <FontAwesomeIcon className="fa-solid photo" icon={faImage} />,
     text: "Photo",
   },
   {
-    icon: <FontAwesomeIcon className="fa-solid" icon={faVideo} />,
+    icon: <FontAwesomeIcon className="fa-solid video" icon={faVideo} />,
     text: "Video",
   },
   {
-    icon: <FontAwesomeIcon className="fa-solid" icon={faCalendar} />,
+    icon: <FontAwesomeIcon className="fa-solid event" icon={faCalendar} />,
     text: "Event",
   },
   {
-    icon: <FontAwesomeIcon className="fa-solid" icon={faArrowAltCircleLeft} />,
+    icon: <FontAwesomeIcon className="fa-solid article" icon={faNewspaper} />,
     text: "Write aritcle",
   },
 ];
@@ -253,28 +254,32 @@ export const WHO_TO_CONNECT = [
   {
     name: "Devesh Verma ",
     username: "deveshverma",
-    avatar: "https://media-exp1.licdn.com/dms/image/C5603AQEc0Tg_g2Mljg/profile-displayphoto-shrink_800_800/0/1645005415410?e=1655337600&v=beta&t=FVJMIiR9RZex3M8nlEmLdNB5b6OmqntHR1ZHT2EH8oQ",
+    avatar:
+      "https://media-exp1.licdn.com/dms/image/C5603AQEc0Tg_g2Mljg/profile-displayphoto-shrink_800_800/0/1645005415410?e=1655337600&v=beta&t=FVJMIiR9RZex3M8nlEmLdNB5b6OmqntHR1ZHT2EH8oQ",
   },
   {
     name: "Deepak Sharma",
     username: "dasjideepak",
-    avatar: "https://media-exp1.licdn.com/dms/image/C4E03AQHSIdC_qQB3Ow/profile-displayphoto-shrink_800_800/0/1628099682694?e=1655337600&v=beta&t=CapQDwp1tUOlBGQRHXYloAq04gMqc3Yy2S_LQvCHHqc",
+    avatar:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQHSIdC_qQB3Ow/profile-displayphoto-shrink_800_800/0/1628099682694?e=1655337600&v=beta&t=CapQDwp1tUOlBGQRHXYloAq04gMqc3Yy2S_LQvCHHqc",
   },
- 
+
   {
     name: "Saurabh Yadav",
     username: "saurabhyadav",
-    avatar: "https://media-exp1.licdn.com/dms/image/C5603AQFFxGzBBkQLtg/profile-displayphoto-shrink_800_800/0/1648116009422?e=1655337600&v=beta&t=OBbOm2MCPh5X0JtyO4vgZAOrRy2B2Ikx5fjDRA-VyvE",
+    avatar:
+      "https://media-exp1.licdn.com/dms/image/C5603AQFFxGzBBkQLtg/profile-displayphoto-shrink_800_800/0/1648116009422?e=1655337600&v=beta&t=OBbOm2MCPh5X0JtyO4vgZAOrRy2B2Ikx5fjDRA-VyvE",
   },
   {
     name: "Sudhanshu kumar",
     username: "sudhanshukumar",
-    avatar: "https://media-exp1.licdn.com/dms/image/C4E03AQGOloPUitz6bw/profile-displayphoto-shrink_800_800/0/1643126839018?e=1655337600&v=beta&t=ctLZiTPArR3NUAm_0InWOg0IAknnc-2tnoudR1SuRDY",
+    avatar:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQGOloPUitz6bw/profile-displayphoto-shrink_800_800/0/1643126839018?e=1655337600&v=beta&t=ctLZiTPArR3NUAm_0InWOg0IAknnc-2tnoudR1SuRDY",
   },
   {
     name: "Abhishek Prajapati",
     username: "abhishekprajapati",
-    avatar: "https://media-exp1.licdn.com/dms/image/D4E35AQFjrxlxPFwB7g/profile-framedphoto-shrink_800_800/0/1626158748115?e=1650088800&v=beta&t=-ZQMEk66wxUNQx6XDiZEKXWbTLmZ7DZJ8rp-JqKVkmE",
+    avatar:
+      "https://media-exp1.licdn.com/dms/image/D4E35AQFjrxlxPFwB7g/profile-framedphoto-shrink_800_800/0/1626158748115?e=1650088800&v=beta&t=-ZQMEk66wxUNQx6XDiZEKXWbTLmZ7DZJ8rp-JqKVkmE",
   },
-  
-]
+];
