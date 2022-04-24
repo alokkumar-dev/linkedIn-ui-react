590f2e7eb352e102e12a1eb4f47d8210b8530af9
alokkumar-dev <alokshakya2601@gmail.com>
2022-04-24 14:31:15 +0530

    add more data

--- src/Data/PostData.js
+++ src/Data/PostData.js
@@ -293,6 +293,287 @@ export const Posts_Data = [
       },
     ],
   },
+  {
+    name: "Prateek Shukla",
+    about_avatar:
+      "Hiring for leadership positions in product & engineering at Masai",
+    avatar:
+      "https://media-exp1.licdn.com/dms/image/C5603AQH6YLJNpKYptw/profile-displayphoto-shrink_800_800/0/1645321272614?e=1655942400&v=beta&t=a-ZWuP6G5_txcvdag26NV7O25xldJEkbV8b-_Tpi6Qs",
+    about_post:
+      "🚨 Announcement 🚨  National Skill Development Corporation 🤝 Masai School to skill 1.5 lacs students of 🇮🇳 What it entails for our students 👇 - All the students who graduate from Masai will be certified by the NSDC at the time of graduation 😍",
+    time: "3w",
+    post_picture:
+      "https://gumlet.assettype.com/freepressjournal/2022-03/8c68c141-fca0-4ebc-beb2-5592d2987ba3/Masai_partners_with_NSDC__image_2.jpg?format=webp&dpr=1.0&q=70&w=768",
+    likes: "Saurabh Yadav and 1,090 others",
+    comments: "45 comments",
+    icons: [
+      {
+        icon: <FontAwesomeIcon icon={faThumbsUp} />,
+        text: "Like",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faCommentDots} />,
+        text: "Comment",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faArrowTurnRight} />,
+        text: "Share",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faPaperPlane} />,
+        text: "Send",
+      },
+    ],
+  },
+  {
+    name: "Yogesh Bhat",
+    about_avatar: "Co-founder & SVP @Masai School |Transformation in Educat.",
+    avatar:
+      "https://media-exp1.licdn.com/dms/image/C5603AQF9Mgs6p2Mbcg/profile-displayphoto-shrink_800_800/0/1624713289242?e=1654732800&v=beta&t=yZ0SO8VCKCCd1BhMRfElFLclDFamfTg_h0SOJDo8HTM",
+    about_post:
+      "# 18 / 101 Power Skills - Active Patience Power skills help individuals to # achieve their potential # achieve their aspirations Active patience demands actions & Good intentions while waiting for results to be achieved.",
+    time: "24h",
+    post_picture:
+      "https://media-exp1.licdn.com/dms/image/C5622AQE-pWsM1rZhWg/feedshare-shrink_800/0/1649867369564?e=2147483647&v=beta&t=YzNkBS3nB_MqADk92m8iSRAW74xQsYKY4xNija-P7f8",
+    likes: "Devesh Verma and 99 others",
+    comments: "20 comments",
+    icons: [
+      {
+        icon: <FontAwesomeIcon icon={faThumbsUp} />,
+        text: "Like",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faCommentDots} />,
+        text: "Comment",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faArrowTurnRight} />,
+        text: "Share",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faPaperPlane} />,
+        text: "Send",
+      },
+    ],
+  },
+  {
+    name: "Deepak Sharma",
+    about_avatar: "Full Stack JavaScript Developer",
+    avatar:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQHSIdC_qQB3Ow/profile-displayphoto-shrink_800_800/0/1628099682694?e=1655337600&v=beta&t=CapQDwp1tUOlBGQRHXYloAq04gMqc3Yy2S_LQvCHHqc",
+    about_post: "First reward from my corporate life #credilio",
+    time: "6mo",
+    post_picture:
+      "https://media-exp1.licdn.com/dms/image/C4E22AQEWaflE7xAVnA/feedshare-shrink_800/0/1637339655972?e=2147483647&v=beta&t=IhAqB8T0D1MfwzGBA_6rZx9orwn3rV77E4-jzrKN2Ms",
+    likes: "Abhishek prajapati and 136 others",
+    comments: "5 comments",
+    icons: [
+      {
+        icon: <FontAwesomeIcon icon={faThumbsUp} />,
+        text: "Like",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faCommentDots} />,
+        text: "Comment",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faArrowTurnRight} />,
+        text: "Share",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faPaperPlane} />,
+        text: "Send",
+      },
+    ],
+  },
+  {
+    name: "Bhabana Samantara",
+    about_avatar:
+      "Frontend web developer at The UniQue Culture| A Multinational Software Development",
+    avatar:
+      "https://media-exp1.licdn.com/dms/image/C5603AQF88c9LoExwbA/profile-displayphoto-shrink_800_800/0/1648892490663?e=1655942400&v=beta&t=WYLNXL41d0MZGhrEumsjrAOiVVMgBDr-FapvhH5NyR0",
+    about_post:
+      "Here is the Certificate of Web development from Hindi Tech Tutorials... Thank you Rajdeep Dhakad sir for being a good mentor and for guiding me on the right path...I will always be grateful to you for your support 🙏🙏😊",
+    time: "1yr",
+    post_picture:
+      "https://media-exp1.licdn.com/dms/image/C5622AQE3LrydUAfdXg/feedshare-shrink_800/0/1620451203123?e=2147483647&v=beta&t=kAjkntY0yh_EvlXNX1Gd1L--Fybm_PByEE5SthKO73s",
+    likes: "You and 26 others",
+    comments: "20 comments",
+    icons: [
+      {
+        icon: <FontAwesomeIcon icon={faThumbsUp} />,
+        text: "Like",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faCommentDots} />,
+        text: "Comment",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faArrowTurnRight} />,
+        text: "Share",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faPaperPlane} />,
+        text: "Send",
+      },
+    ],
+  },
+  {
+    name: "Gurdeep Singh",
+    about_avatar: "Associate SDE | Actyv.ai",
+    avatar:
+      "https://media-exp1.licdn.com/dms/image/C5603AQF2N5ET-q-bzg/profile-displayphoto-shrink_800_800/0/1638970930286?e=1655942400&v=beta&t=n4RBYqMEN3Pa0lavLRryvG5RYaxcY6W1zL8dfRMa2BE",
+    about_post:
+      "I'm delighted to share with you that I have joined Actyv.ai® as an Associate SDE.Thanks, Masai School for making my dream True.Special thanks to co-founders Nrupul Dev, Yogesh Bhat, and Prateek Shukla for creating such an amazing platform to create and grow your skills.",
+    time: "2w",
+    post_picture:
+      "https://media-exp1.licdn.com/dms/image/C5622AQFw2DHgw6Bv5Q/feedshare-shrink_800/0/1649414144325?e=2147483647&v=beta&t=SdidsBK2MgrI1aLzQK1PuvlmBBemUd4sD1Fkv9UE1G0",
+    likes: "Sudhanshu Kumar and 20 others",
+    comments: "12 comments",
+    icons: [
+      {
+        icon: <FontAwesomeIcon icon={faThumbsUp} />,
+        text: "Like",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faCommentDots} />,
+        text: "Comment",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faArrowTurnRight} />,
+        text: "Share",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faPaperPlane} />,
+        text: "Send",
+      },
+    ],
+  },
+  {
+    name: "Manthan Gaikwad",
+    about_avatar: "Associate Software Engineer @ Indus Net Technologies",
+    avatar:
+      "https://media-exp1.licdn.com/dms/image/C5603AQGDju7zb2KCGA/profile-displayphoto-shrink_800_800/0/1648480695796?e=1655942400&v=beta&t=zACJq-FApJKhwOF46gYfjm8FM8NqP4CbV-znSz9edeg",
+    about_post:
+      "#15 / 101 Power Skills -  Marathon Mindset  Power skills help individuals to # achieve their potential # achieve their aspirations Life is a marathon, not a sprint; pace yourself accordingly -Amby B A marathon mindset means that",
+    time: "2w",
+    post_picture:
+      "https://media.licdn.com/media/AAYQAQQSAAgAAQAAAAAAABqdcB5EZ18gQPCF4HzBOFBBhw.gif",
+    likes: "Kapil Mogre and 165 others",
+    comments: "20 comments",
+    icons: [
+      {
+        icon: <FontAwesomeIcon icon={faThumbsUp} />,
+        text: "Like",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faCommentDots} />,
+        text: "Comment",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faArrowTurnRight} />,
+        text: "Share",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faPaperPlane} />,
+        text: "Send",
+      },
+    ],
+  },
+  {
+    name: "Akshay Saini",
+    about_avatar: "Engineer at Uber | Teacher | YouTuber",
+    avatar:
+      "https://media-exp1.licdn.com/dms/image/C5103AQGDGDnudfmZTQ/profile-displayphoto-shrink_800_800/0/1579620531120?e=1655942400&v=beta&t=MuC4uE73lP3lr_QgK_ZI2g38BK5J4CLPKG09-Uy9PBg",
+    about_post:
+      "Wohooo, what a meet-up it was in Hyderabad!! 🤩🤩 Thank you for taking out time and joining,it was just amazing meeting you in person. 😇",
+    time: "2w",
+    post_picture:
+      "https://media-exp1.licdn.com/dms/image/C4D22AQHeoiyw9PReQw/feedshare-shrink_2048_1536/0/1650335402761?e=2147483647&v=beta&t=VBjzMsubTfOYEdamVPNg6DVy8rHCCRI_w331dDUE808",
+    likes: "You and 6,220 others",
+    comments: "23 comments",
+    icons: [
+      {
+        icon: <FontAwesomeIcon icon={faThumbsUp} />,
+        text: "Like",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faCommentDots} />,
+        text: "Comment",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faArrowTurnRight} />,
+        text: "Share",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faPaperPlane} />,
+        text: "Send",
+      },
+    ],
+  },
+  {
+    name: "Abhishek Maurya",
+    about_avatar:
+      "Instructional Associate at Masai School | Full-Stack Dev | Trader | Investor",
+    avatar:
+      "https://media-exp1.licdn.com/dms/image/C5603AQFo1igfd4cyAw/profile-displayphoto-shrink_800_800/0/1626527277875?e=1655942400&v=beta&t=z9PqTdCh2gKC3EU3J4gFdz1iz1_dMYDCGwk1LTvaD5I",
+    about_post:
+      "Hello fam, I am delighted to share that I have joined Masai School as an Instructional Associate. I always loved the environment, culture and team spirit her",
+    time: "2mo",
+    post_picture:
+      "https://media.licdn.com/media/AAYQAQQSAAgAAQAAAAAAABwEEXEeLoHISM2cU9LdzliLaQ.gif",
+    likes: "Rohan Verma and 130 others",
+    comments: "30 comments",
+    icons: [
+      {
+        icon: <FontAwesomeIcon icon={faThumbsUp} />,
+        text: "Like",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faCommentDots} />,
+        text: "Comment",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faArrowTurnRight} />,
+        text: "Share",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faPaperPlane} />,
+        text: "Send",
+      },
+    ],
+  },
+  {
+    name: "Sheelu Kumar",
+    about_avatar: "Full Stack Dev | Instructional associate @masaischool",
+    avatar:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQGqAMHXmA8HdQ/profile-displayphoto-shrink_800_800/0/1631989874549?e=1655942400&v=beta&t=z8AItpnS4y-3HZaL4M4tGwiYSr49osb4FWB-r4fzzL0",
+    about_post:
+      "Hello everyone, I feel really blessed to share that I have now Joined Masai School as an Instructional Associate (IA).It have been a wonderful journey in masai and thanks to all the support and training given by Nrupul Dev Yogesh Bhat Prateek Shukla Chandra Sekhar Swanand Kadam Dhaval Chheda Prabhanjan Padhye  Neeraj Malwal and all Masai School operation team.",
+    time: "1mo",
+    post_picture:
+      "https://media.licdn.com/media/AAYQAQQSAAgAAQAAAAAAABqdcB5EZ18gQPCF4HzBOFBBhw.gif",
+    likes: "You and 120 others",
+    comments: "25 comments",
+    icons: [
+      {
+        icon: <FontAwesomeIcon icon={faThumbsUp} />,
+        text: "Like",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faCommentDots} />,
+        text: "Comment",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faArrowTurnRight} />,
+        text: "Share",
+      },
+      {
+        icon: <FontAwesomeIcon icon={faPaperPlane} />,
+        text: "Send",
+      },
+    ],
+  },
 ];
 
 export const NAVBAR_ICONS = [
