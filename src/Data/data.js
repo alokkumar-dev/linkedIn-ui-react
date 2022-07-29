3965a3dfb1cd1cbaf542aec62c1aa709d1fe323e
Alok Kumar <71998637+alokkumar-dev@users.noreply.github.com>
2022-07-29 12:08:58 +0530

    update data post and avatar images url

--- src/Data/data.js
+++ src/Data/data.js
@@ -16,7 +16,7 @@ export const Posts_Data = [
     about_avatar:
       "Hiring for leadership positions in product & engineering at Masai",
     avatar:
-      "https://media-exp1.licdn.com/dms/image/C5603AQH6YLJNpKYptw/profile-displayphoto-shrink_800_800/0/1645321272614?e=1655942400&v=beta&t=a-ZWuP6G5_txcvdag26NV7O25xldJEkbV8b-_Tpi6Qs",
+      "https://media-exp1.licdn.com/dms/image/C5603AQH6YLJNpKYptw/profile-displayphoto-shrink_800_800/0/1645321272614?e=1664409600&v=beta&t=hZliAAQkjYW4qp42vV-S-dKzDBg0oh2HIGJJKq_cZQQ",
     about_post:
       "🚨 Announcement 🚨  National Skill Development Corporation 🤝 Masai School to skill 1.5 lacs students of 🇮🇳 What it entails for our students 👇 - All the students who graduate from Masai will be certified by the NSDC at the time of graduation 😍",
     time: "3w",
@@ -47,7 +47,7 @@ export const Posts_Data = [
     name: "Yogesh Bhat",
     about_avatar: "Co-founder & SVP @Masai School |Transformation in Educat.",
     avatar:
-      "https://media-exp1.licdn.com/dms/image/C5603AQF9Mgs6p2Mbcg/profile-displayphoto-shrink_800_800/0/1624713289242?e=1654732800&v=beta&t=yZ0SO8VCKCCd1BhMRfElFLclDFamfTg_h0SOJDo8HTM",
+      "https://media-exp1.licdn.com/dms/image/C4D03AQGDD9G1H0asoA/profile-displayphoto-shrink_800_800/0/1655700768997?e=1664409600&v=beta&t=hccNXcYXNwwVqbvhjtLSjDgVXCMgyUQGQllxem-oNAA",
     about_post:
       "# 18 / 101 Power Skills - Active Patience Power skills help individuals to # achieve their potential # achieve their aspirations Active patience demands actions & Good intentions while waiting for results to be achieved.",
     time: "24h",
@@ -78,7 +78,7 @@ export const Posts_Data = [
     name: "Deepak Sharma",
     about_avatar: "Full Stack JavaScript Developer",
     avatar:
-      "https://media-exp1.licdn.com/dms/image/C4E03AQHSIdC_qQB3Ow/profile-displayphoto-shrink_800_800/0/1628099682694?e=1655337600&v=beta&t=CapQDwp1tUOlBGQRHXYloAq04gMqc3Yy2S_LQvCHHqc",
+      "https://media-exp1.licdn.com/dms/image/D5635AQGbQcJ0dKMVMg/profile-framedphoto-shrink_800_800/0/1658344383343?e=1659682800&v=beta&t=z4x-G-paCRt71F3qjttZ81v40sTFRB67-Ob-2FEYz7A",
     about_post: "First reward from my corporate life #credilio",
     time: "6mo",
     post_picture:
@@ -390,7 +390,7 @@ export const Posts_Data = [
     about_avatar:
       "Frontend web developer at The UniQue Culture| A Multinational Software Development",
     avatar:
-      "https://media-exp1.licdn.com/dms/image/C5603AQF88c9LoExwbA/profile-displayphoto-shrink_800_800/0/1648892490663?e=1655942400&v=beta&t=WYLNXL41d0MZGhrEumsjrAOiVVMgBDr-FapvhH5NyR0",
+      "https://media-exp1.licdn.com/dms/image/C5603AQF88c9LoExwbA/profile-displayphoto-shrink_800_800/0/1648892490663?e=1664409600&v=beta&t=eenM2NCjztq7wNqA-GIBRx6A_KsDyAh3Zc6reCMFGOA",
     about_post:
       "Here is the Certificate of Web development from Hindi Tech Tutorials... Thank you Rajdeep Dhakad sir for being a good mentor and for guiding me on the right path...I will always be grateful to you for your support 🙏🙏😊",
     time: "1yr",
@@ -421,12 +421,12 @@ export const Posts_Data = [
     name: "Gurdeep Singh",
     about_avatar: "Associate SDE | Actyv.ai",
     avatar:
-      "https://media-exp1.licdn.com/dms/image/C5603AQF2N5ET-q-bzg/profile-displayphoto-shrink_800_800/0/1638970930286?e=1655942400&v=beta&t=n4RBYqMEN3Pa0lavLRryvG5RYaxcY6W1zL8dfRMa2BE",
+      "https://media-exp1.licdn.com/dms/image/C5603AQF2N5ET-q-bzg/profile-displayphoto-shrink_400_400/0/1638970930286?e=1664409600&v=beta&t=LrK0Xav2_fPdc8-RJg8wWx-b4aWnmF3jc--lfCwAxKI",
     about_post:
       "I'm delighted to share with you that I have joined Actyv.ai® as an Associate SDE.Thanks, Masai School for making my dream True.Special thanks to co-founders Nrupul Dev, Yogesh Bhat, and Prateek Shukla for creating such an amazing platform to create and grow your skills.",
     time: "2w",
     post_picture:
-      "https://media-exp1.licdn.com/dms/image/C5622AQFw2DHgw6Bv5Q/feedshare-shrink_800/0/1649414144325?e=2147483647&v=beta&t=SdidsBK2MgrI1aLzQK1PuvlmBBemUd4sD1Fkv9UE1G0",
+      "https://media-exp1.licdn.com/dms/image/C5622AQFw2DHgw6Bv5Q/feedshare-shrink_800/0/1649414144325?e=1661990400&v=beta&t=KrBdHiF2ilF3VZohskJinTZnOcIPuj0P2q_nWpKAF9A",
     likes: "Sudhanshu Kumar and 20 others",
     comments: "12 comments",
     icons: [
@@ -452,7 +452,7 @@ export const Posts_Data = [
     name: "Manthan Gaikwad",
     about_avatar: "Associate Software Engineer @ Indus Net Technologies",
     avatar:
-      "https://media-exp1.licdn.com/dms/image/C5603AQGDju7zb2KCGA/profile-displayphoto-shrink_800_800/0/1648480695796?e=1655942400&v=beta&t=zACJq-FApJKhwOF46gYfjm8FM8NqP4CbV-znSz9edeg",
+      "https://media-exp1.licdn.com/dms/image/C5603AQGDju7zb2KCGA/profile-displayphoto-shrink_400_400/0/1648480695796?e=1664409600&v=beta&t=S1TTrrLQ9AQR_q9-WLeduayszo6JIz_woJyhf9mse6k",
     about_post:
       "#15 / 101 Power Skills -  Marathon Mindset  Power skills help individuals to # achieve their potential # achieve their aspirations Life is a marathon, not a sprint; pace yourself accordingly -Amby B A marathon mindset means that",
     time: "2w",
@@ -483,12 +483,12 @@ export const Posts_Data = [
     name: "Akshay Saini",
     about_avatar: "Engineer at Uber | Teacher | YouTuber",
     avatar:
-      "https://media-exp1.licdn.com/dms/image/C5103AQGDGDnudfmZTQ/profile-displayphoto-shrink_800_800/0/1579620531120?e=1655942400&v=beta&t=MuC4uE73lP3lr_QgK_ZI2g38BK5J4CLPKG09-Uy9PBg",
+      "https://media-exp1.licdn.com/dms/image/C5103AQGDGDnudfmZTQ/profile-displayphoto-shrink_400_400/0/1579620531120?e=1664409600&v=beta&t=zIhgmruxGAL2t1eCXvUHejYcrZ9v60Te-pCXwKm7CDg",
     about_post:
-      "Wohooo, what a meet-up it was in Hyderabad!! 🤩🤩 Thank you for taking out time and joining,it was just amazing meeting you in person. 😇",
+      "Do you wanna fall in love with JavaScript? Watch this FREE series on YouTube. ❤️",
     time: "2w",
     post_picture:
-      "https://media-exp1.licdn.com/dms/image/C4D22AQHeoiyw9PReQw/feedshare-shrink_2048_1536/0/1650335402761?e=2147483647&v=beta&t=VBjzMsubTfOYEdamVPNg6DVy8rHCCRI_w331dDUE808",
+      "https://media-exp1.licdn.com/dms/image/sync/C4D27AQF9wM9IglJxtg/articleshare-shrink_800/0/1658060311170?e=1659682800&v=beta&t=Af9cVUVd3Dgq-qtRoOOgVrCUzF2PhcX0JjSrb5fuJ0o",
     likes: "You and 6,220 others",
     comments: "23 comments",
     icons: [
@@ -515,7 +515,7 @@ export const Posts_Data = [
     about_avatar:
       "Instructional Associate at Masai School | Full-Stack Dev | Trader | Investor",
     avatar:
-      "https://media-exp1.licdn.com/dms/image/C5603AQFo1igfd4cyAw/profile-displayphoto-shrink_800_800/0/1626527277875?e=1655942400&v=beta&t=z9PqTdCh2gKC3EU3J4gFdz1iz1_dMYDCGwk1LTvaD5I",
+      "https://media-exp1.licdn.com/dms/image/C5603AQFo1igfd4cyAw/profile-displayphoto-shrink_800_800/0/1626527277875?e=1664409600&v=beta&t=pQSdlok2164pFV2DEfipVCsSW6JNeYvtQwFroZ3kYpk",
     about_post:
       "Hello fam, I am delighted to share that I have joined Masai School as an Instructional Associate. I always loved the environment, culture and team spirit her",
     time: "2mo",
@@ -546,7 +546,7 @@ export const Posts_Data = [
     name: "Sheelu Kumar",
     about_avatar: "Full Stack Dev | Instructional associate @masaischool",
     avatar:
-      "https://media-exp1.licdn.com/dms/image/C4E03AQGqAMHXmA8HdQ/profile-displayphoto-shrink_800_800/0/1631989874549?e=1655942400&v=beta&t=z8AItpnS4y-3HZaL4M4tGwiYSr49osb4FWB-r4fzzL0",
+      "https://media-exp1.licdn.com/dms/image/C4E03AQGqAMHXmA8HdQ/profile-displayphoto-shrink_800_800/0/1631989874549?e=1664409600&v=beta&t=EkIi81dWFaoE1QLEarBk1kmnDkxPRWzbCvWIi-MG4gM",
     about_post:
       "Hello everyone, I feel really blessed to share that I have now Joined Masai School as an Instructional Associate (IA).It have been a wonderful journey in masai and thanks to all the support and training given by Nrupul Dev Yogesh Bhat Prateek Shukla Chandra Sekhar Swanand Kadam Dhaval Chheda Prabhanjan Padhye  Neeraj Malwal and all Masai School operation team.",
     time: "1mo",
@@ -605,7 +605,7 @@ export const WHO_TO_CONNECT = [
     name: "Deepak Sharma",
     username: "dasjideepak",
     avatar:
-      "https://media-exp1.licdn.com/dms/image/D4D35AQGHFFp7iDyCzQ/profile-framedphoto-shrink_100_100/0/1655316052951?e=1658750400&v=beta&t=iPFE8ldmqFgnyAAnEA7AQi4u1GAuRTX1C1U3V_51NOY",
+      "https://media-exp1.licdn.com/dms/image/D5635AQGbQcJ0dKMVMg/profile-framedphoto-shrink_800_800/0/1658344383343?e=1659682800&v=beta&t=z4x-G-paCRt71F3qjttZ81v40sTFRB67-Ob-2FEYz7A",
   },
 
   {
